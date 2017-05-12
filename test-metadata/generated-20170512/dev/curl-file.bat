ec46548183a3a433155c766d92abcb8e08823ef8
pappleby <pauldappleby@gmail.com>
2017-05-12 23:54:21 +0100

    Ignore

---
+++ test-metadata/generated-20170512/dev/curl-file.bat
@@ -0,0 +1,109 @@
+md results
+timeout 1
+echo 1 of 36
+curl -X POST -d @json/work/work-ab5a18ce-7412-4ec8-8f26-3e469434ec00.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-ab5a18ce-7412-4ec8-8f26-3e469434ec00.txt -i
+timeout 1
+echo 2 of 36
+curl -X POST -d @json/manifestation/manifestation-73eb4b08-43c4-4826-a318-aac4e79ec3e8.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-73eb4b08-43c4-4826-a318-aac4e79ec3e8.txt -i
+timeout 1
+echo 3 of 36
+curl -X POST -d @json/manifestation/manifestation-d6f23f90-828b-4aa2-b364-4fee2e0975df.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-d6f23f90-828b-4aa2-b364-4fee2e0975df.txt -i
+timeout 1
+echo 4 of 36
+curl -X POST -d @json/manifestation/manifestation-b177eca3-e947-4c80-b575-8957731670ea.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-b177eca3-e947-4c80-b575-8957731670ea.txt -i
+timeout 1
+echo 5 of 36
+curl -X POST -d @json/work/work-ec35ab18-14d4-4e62-acb3-3dd87ee2510c.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-ec35ab18-14d4-4e62-acb3-3dd87ee2510c.txt -i
+timeout 1
+echo 6 of 36
+curl -X POST -d @json/manifestation/manifestation-5c6084b1-3b6d-45f9-9a4c-0e58e248d6ff.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-5c6084b1-3b6d-45f9-9a4c-0e58e248d6ff.txt -i
+timeout 1
+echo 7 of 36
+curl -X POST -d @json/manifestation/manifestation-9bc6b4e0-e1d7-4512-b808-98dc3c3a7eef.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-9bc6b4e0-e1d7-4512-b808-98dc3c3a7eef.txt -i
+timeout 1
+echo 8 of 36
+curl -X POST -d @json/work/work-51b088f6-b458-449b-98c4-be63ff59ba4e.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-51b088f6-b458-449b-98c4-be63ff59ba4e.txt -i
+timeout 1
+echo 9 of 36
+curl -X POST -d @json/manifestation/manifestation-3e47ca3c-835a-423f-bc0e-410d2b8aba78.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-3e47ca3c-835a-423f-bc0e-410d2b8aba78.txt -i
+timeout 1
+echo 10 of 36
+curl -X POST -d @json/work/work-2bccdd91-0f0c-4fbc-8cf3-83d09078b15d.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-2bccdd91-0f0c-4fbc-8cf3-83d09078b15d.txt -i
+timeout 1
+echo 11 of 36
+curl -X POST -d @json/manifestation/manifestation-7a92d2fd-7ed6-441d-a620-9a75e4f190b6.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-7a92d2fd-7ed6-441d-a620-9a75e4f190b6.txt -i
+timeout 1
+echo 12 of 36
+curl -X POST -d @json/manifestation/manifestation-dd30a49f-54a6-424e-850a-fa6c475e768e.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-dd30a49f-54a6-424e-850a-fa6c475e768e.txt -i
+timeout 1
+echo 13 of 36
+curl -X POST -d @json/manifestation/manifestation-3a5a387a-e5ad-4913-8790-12a9b99309b2.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-3a5a387a-e5ad-4913-8790-12a9b99309b2.txt -i
+timeout 1
+echo 14 of 36
+curl -X POST -d @json/manifestation/manifestation-d70b87ed-aa06-4b71-aa92-86f67a54208f.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-d70b87ed-aa06-4b71-aa92-86f67a54208f.txt -i
+timeout 1
+echo 15 of 36
+curl -X POST -d @json/work/work-3a56347f-1b0e-4e5f-af9d-9d2aac2781f7.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-3a56347f-1b0e-4e5f-af9d-9d2aac2781f7.txt -i
+timeout 1
+echo 16 of 36
+curl -X POST -d @json/manifestation/manifestation-dee980f0-258b-47d8-8c97-d0113846ae5e.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-dee980f0-258b-47d8-8c97-d0113846ae5e.txt -i
+timeout 1
+echo 17 of 36
+curl -X POST -d @json/manifestation/manifestation-2376a910-2e9c-4840-be77-8f2598f73bf7.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-2376a910-2e9c-4840-be77-8f2598f73bf7.txt -i
+timeout 1
+echo 18 of 36
+curl -X POST -d @json/manifestation/manifestation-875aab00-6cd5-4a41-8b21-556be1d68e39.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-875aab00-6cd5-4a41-8b21-556be1d68e39.txt -i
+timeout 1
+echo 19 of 36
+curl -X POST -d @json/work/work-891cc58c-179f-4e2b-aef5-3e001384d57f.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-891cc58c-179f-4e2b-aef5-3e001384d57f.txt -i
+timeout 1
+echo 20 of 36
+curl -X POST -d @json/manifestation/manifestation-73554d39-56e1-4c10-9c32-70a3d19505bb.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-73554d39-56e1-4c10-9c32-70a3d19505bb.txt -i
+timeout 1
+echo 21 of 36
+curl -X POST -d @json/manifestation/manifestation-5fb61608-1e93-4cea-b5c1-ea9ac221d76c.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-5fb61608-1e93-4cea-b5c1-ea9ac221d76c.txt -i
+timeout 1
+echo 22 of 36
+curl -X POST -d @json/manifestation/manifestation-51305e54-314f-4ca8-acf1-c97f5208642d.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-51305e54-314f-4ca8-acf1-c97f5208642d.txt -i
+timeout 1
+echo 23 of 36
+curl -X POST -d @json/work/work-8d87c5e9-30ef-4fd5-84c9-1fe2883a3d2f.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-8d87c5e9-30ef-4fd5-84c9-1fe2883a3d2f.txt -i
+timeout 1
+echo 24 of 36
+curl -X POST -d @json/manifestation/manifestation-70f22849-44f7-4e82-91c1-d3a08b110610.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-70f22849-44f7-4e82-91c1-d3a08b110610.txt -i
+timeout 1
+echo 25 of 36
+curl -X POST -d @json/manifestation/manifestation-99c6b340-a6f3-4b45-80d4-0f9c6b13d594.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-99c6b340-a6f3-4b45-80d4-0f9c6b13d594.txt -i
+timeout 1
+echo 26 of 36
+curl -X POST -d @json/manifestation/manifestation-8e117156-b079-4ce7-8857-59e4839a1b8d.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-8e117156-b079-4ce7-8857-59e4839a1b8d.txt -i
+timeout 1
+echo 27 of 36
+curl -X POST -d @json/work/work-a291488a-4392-4a40-a09c-fab3adf9a277.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-a291488a-4392-4a40-a09c-fab3adf9a277.txt -i
+timeout 1
+echo 28 of 36
+curl -X POST -d @json/work/work-39b6f301-c8bf-4a90-bd2b-5cd2b89c8681.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-39b6f301-c8bf-4a90-bd2b-5cd2b89c8681.txt -i
+timeout 1
+echo 29 of 36
+curl -X POST -d @json/manifestation/manifestation-530c9ef4-43b5-462a-b0e4-336e8ad41310.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-530c9ef4-43b5-462a-b0e4-336e8ad41310.txt -i
+timeout 1
+echo 30 of 36
+curl -X POST -d @json/manifestation/manifestation-a68af93f-b724-4d27-9ae5-17a92c536536.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-a68af93f-b724-4d27-9ae5-17a92c536536.txt -i
+timeout 1
+echo 31 of 36
+curl -X POST -d @json/manifestation/manifestation-13ba57a3-fba8-492d-91b2-2036d65f171f.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-13ba57a3-fba8-492d-91b2-2036d65f171f.txt -i
+timeout 1
+echo 32 of 36
+curl -X POST -d @json/work/work-3c99d5f3-4885-4985-8c57-d411a6a23403.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-3c99d5f3-4885-4985-8c57-d411a6a23403.txt -i
+timeout 1
+echo 33 of 36
+curl -X POST -d @json/manifestation/manifestation-5283cb85-3065-4582-8c52-fcc67ea53fb4.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-5283cb85-3065-4582-8c52-fcc67ea53fb4.txt -i
+timeout 1
+echo 34 of 36
+curl -X POST -d @json/manifestation/manifestation-d0b9b70f-8e10-4abc-9326-f8bfd597811b.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-d0b9b70f-8e10-4abc-9326-f8bfd597811b.txt -i
+timeout 1
+echo 35 of 36
+curl -X POST -d @json/manifestation/manifestation-12f0146c-a4cd-4d8b-9f07-717a8b133917.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-12f0146c-a4cd-4d8b-9f07-717a8b133917.txt -i
+timeout 1
+echo 36 of 36
+curl -X POST -d @json/manifestation/manifestation-99882d3b-cff6-46c8-a67e-445a3165d429.json -H "Content-Type: application/json" -H "Authorization: Basic Ymx1ZWJlcnJ5OmVAQkhSTUF2M2V5S2xiT1VjS0tAWl56Q0ZhMDRtYw==" -H "X-Roles: LearningAdmin,ContentMetadataAdmin" -k https://develop-data.pearsoncms.net/api/api/thing?db=qa0 -o results/results-99882d3b-cff6-46c8-a67e-445a3165d429.txt -i
